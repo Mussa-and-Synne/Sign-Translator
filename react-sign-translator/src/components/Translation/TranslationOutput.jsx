397e8c614a6441191cde19158ee27953524291be
Synne Sævik <synne.savik@no.experis.com>
2022-09-20 08:24:27 +0200

    fix: moved img in folder

--- react-sign-translator/src/components/Translation/TranslationOutput.jsx
+++ react-sign-translator/src/components/Translation/TranslationOutput.jsx
@@ -1,6 +1,6 @@
 const TranslationOutput = (props) => {
     return(
-        <img src={`img/${props.data}.png`} alt={props.data}/>
+        <img src={`img/letters/${props.data}.png`} alt={props.data}/>
     )
 }
 export default TranslationOutput
