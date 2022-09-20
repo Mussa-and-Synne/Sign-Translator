68dd653334d551e405e845057bd07989fa501176
Synne Sævik <synne.savik@no.experis.com>
2022-09-20 08:48:01 +0200

    feat: login css added

--- react-sign-translator/src/views/Login.jsx
+++ react-sign-translator/src/views/Login.jsx
@@ -3,8 +3,9 @@ const Login = () =>{
     return(
         <>
         <header className="login-head">
-            <img src="" alt="" />
-            <h1>Get started!</h1>
+            <img className='logo-hello' src="img/Logo-Hello.png" alt="logo-hello" width="300px" />
+            <h1 className="login-text-h1">Lost in Translation</h1>
+            <h2 className="login-text-h2">Get started!</h2>
         </header>
         <LoginForm/>
         </>
